2b11c393192b8bc8b606f836d2b0ca0429a6c6b7
saikarthikkumar <saikarthik.vodela@sciits.com>
2017-04-19 15:26:42 +0530

    Uploading Page object file
    Uploading Page object file

---
+++ src/test/java/com/sci/testamigo/usermanagement/pageobjects/EditProfile.java
@@ -0,0 +1,148 @@
+/**
+ * @author Poornima
+ *
+ */
+package com.sci.testamigo.usermanagement.pageobjects;
+
+import org.openqa.selenium.WebElement;
+
+import com.sci.testamigo.usermanagement.model.WebElementDetails;
+import com.sci.testamigo.usermanagement.util.JWebElement;
+
+
+public class EditProfile {
+
+  // ==================================================================================
+
+  public WebElement menuSCIUserBtn(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement editProfileBtn(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement changePwdBtn(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement firstNameTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement middletNameTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement lastNameTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement emailTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement mobileNOTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement address1TxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement address2TxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement cityTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement stateTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement countryDropdown(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement postalTXtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement updateBtn(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement cancelBtn(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement errorMessage(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement updateMessage(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement oldPwdTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement newPwdTxtBox(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement confirmNewPwd(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+
+  public WebElement changePwdSaveBtn(WebElementDetails webElementDetails) {
+    return JWebElement.getWebElement(webElementDetails);
+  }
+
+  // ==================================================================================
+}
