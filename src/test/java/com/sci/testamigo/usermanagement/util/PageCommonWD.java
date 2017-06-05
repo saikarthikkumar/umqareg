d98eaeab41e2e4a2a0585f8053cb7ce47f76789c
saikarthikkumar <saikarthik.vodela@sciits.com>
2017-06-05 15:54:00 +0530

    Udpated the file
    Udpated the file

--- src/test/java/com/sci/testamigo/usermanagement/util/PageCommonWD.java
+++ src/test/java/com/sci/testamigo/usermanagement/util/PageCommonWD.java
@@ -24,28 +24,25 @@ public class PageCommonWD {
   private PageCommonWD() {
     resourceBundle = ResourceBundle.getBundle("ApplicationResources");
     if (resourceBundle.getString(Constants.BROWSER_NAME).equalsIgnoreCase("firefox")) {
-      //Setup firefox binary to start in Xvfb        
+      // Setup firefox binary to start in Xvfb        
         String Xport = System.getProperty("lmportal.xvfb.id", ":1");
         final File firefoxPath = new File(System.getProperty("lmportal.deploy.firefox.path", "/usr/bin/firefox"));
         FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
         firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
         driver = new FirefoxDriver(firefoxBinary, null);
-    	//System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//drivers//geckodriver");
-    	//this.driver = new FirefoxDriver();
+    	//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//drivers//chromedriver.exe");
     } else if (resourceBundle.getString(Constants.BROWSER_NAME).equalsIgnoreCase("chrome")) {
-	   /* String Xport = System.getProperty("lmportal.xvfb.id", ":1");
+	    String Xport = System.getProperty("lmportal.xvfb.id", ":1");
       ChromeDriverService chromeDriverService = new ChromeDriverService.Builder()
     	        .usingDriverExecutable(new File("/usr/bin/google-chrome"))
-    	        .usingAnyFreePort().withEnvironment(ImmutableMap.of("DISPLAY", Xport)).build();*/
-    	/*try {
+    	        .usingAnyFreePort().withEnvironment(ImmutableMap.of("DISPLAY", Xport)).build();
+    	try {
 			chromeDriverService.start();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	driver = new ChromeDriver(chromeDriverService);*/
-    	//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//drivers//chromedriver.exe");
-    	//this.driver = new ChromeDriver();
+    	driver = new ChromeDriver(chromeDriverService);
     }
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.manage().window().maximize();
